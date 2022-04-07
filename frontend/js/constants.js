841bcfda974daa06039830bc686d0c6d906ffacb
Liew Chee Hin <chliew@cmg.com.my>
2022-04-07 15:57:50 +0800

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x8D47730e57Cb2441c4E5e2Fb7bE9eE689891c9ac"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
